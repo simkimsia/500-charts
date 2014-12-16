4c6901213c0564e0fc863413120abadef3caa5e8
KimSia Sim <kimcity@gmail.com>
2014-12-17 07:15:18 +0800

    added counters

--- bni/inputcontroller.js
+++ bni/inputcontroller.js
@@ -11,4 +11,27 @@ $(document).ready(function () {
             'Tong Puay Fun'
         ];
     }
+
+    var niSpinner = new wijmo.input.InputNumber('#theGuestCounter', {
+        min: 0,
+        max: 100,
+        format: 'n0',
+        step: 1,
+        placeholder: 'integer between zero and hundred'
+    });
+
+    var referralSpinner = new wijmo.input.InputNumber('#theReferralCounter', {
+        min: 0,
+        max: 100,
+        format: 'n0',
+        step: 1,
+        placeholder: 'integer between zero and hundred'
+    });
+
+    var niAmount = new wijmo.input.InputNumber('#theDoneDealCounter', {
+        format: 'c',
+        step: 100,
+        max: 1000000,
+        placeholder: 'amount below $1,000,000'
+    });
 });
