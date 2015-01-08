57d29021e3c9bf656c5f0bdcca64fb2f5eb3edd9
KimSia Sim <kimcity@gmail.com>
2015-01-08 11:53:28 +0800

    go back to parseDate

--- chart-area/scripts/app.js
+++ chart-area/scripts/app.js
@@ -8,32 +8,17 @@ var app = angular.module('app', [
 // app controller provides data
 app.controller('appCtrl', ['$scope', 'Api', function ($scope, Api) {
     var request = Api.getData();
-    console.log(request);
-    // generate some random data
-    // var countries = 'US,Germany,UK,Japan,Italy,Greece'.split(','),
-    //     data = [];
-
-    // for (var i = 0; i < countries.length; i++) {
-    //     data.push({
-    //         country: countries[i],
-    //         downloads: Math.round(Math.random() * 20000),
-    //         sales: Math.random() * 10000,
-    //         expenses: Math.random() * 5000
-    //     });
-    // }
+    
     request.$promise.then(function(response) {
         var data = [];
         for (var i = 0; i < response.data.length; i++) {
             data.push({
-                //timestamp: wijmo.Globalize.parseDate(response.data[i].timestamp, 'yyyy-MM-dd HH:mm:ss.ffffff'),
-                timestamp: wijmo.changeType(response.data[i].timestamp, 'Date'),
+                timestamp: wijmo.Globalize.parseDate(response.data[i].timestamp, 'yyyy-MM-dd HH:mm:ss.ffffff'),
                 os: response.data[i].os,
                 sg: response.data[i].sg
             });
         }
         $scope.data = data;
-        console.log($scope.data);
-
     });
 
     // add chart properties to scope
