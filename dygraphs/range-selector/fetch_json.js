c306d837bb38cc4ef5bad4f47e75dcdbf58c81d5
KimSia Sim <kimcity@gmail.com>
2015-03-03 09:39:04 +0800

    rewrite how to fetch json data

--- dygraphs/range-selector/fetch_json.js
+++ dygraphs/range-selector/fetch_json.js
@@ -1,8 +1,12 @@
-var data_json = [];
+function getJSON() {
+	var json;
+	return JSON.parse($.ajax({
+		type: "GET",
+		url: "http://simkimsia.github.io/500-charts/dygraphs/range-selector/data.json",
+		dataType: "json",
+		success: function( data ) {
+			return data;
+		}
+	}).responseText);
 
-$.getJSON(
-	"http://simkimsia.github.io/500-charts/dygraphs/range-selector/data.json", 
-	function( data ) {
-		data_json = data;
-	}
-);
+}
