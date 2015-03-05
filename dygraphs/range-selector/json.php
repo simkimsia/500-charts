fbab7cf4d85487367444b469c25330785383558f
KimSia Sim <kimcity@gmail.com>
2015-03-05 14:22:22 +0800

    change to file_get_contents instead

--- dygraphs/range-selector/json.php
+++ dygraphs/range-selector/json.php
@@ -4,21 +4,7 @@ header('Content-Type: application/json');
 
 $url = "http://tamtech.ddns.net/mktshare/json/?start=20150107&end=20150108&minutes=60&display=%";
 
-//  Initiate curl
-$ch = curl_init();
-// Disable SSL verification
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
-
-// Will dump a beauty json :3
-//var_dump(json_decode($result, true));
+$result = file_get_contents($url);
 
 echo $result;
 ?>
