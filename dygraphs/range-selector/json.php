073ab065aa8da4c2765770b7412373e711b6e34d
KimSia Sim <kimcity@gmail.com>
2015-03-05 14:20:57 +0800

    rewrite the json code

--- dygraphs/range-selector/json.php
+++ dygraphs/range-selector/json.php
@@ -1,17 +1,24 @@
 <?php
 
 header('Content-Type: application/json');
-// create a new cURL resource
+
+$url = "http://tamtech.ddns.net/mktshare/json/?start=20150107&end=20150108&minutes=60&display=%";
+
+//  Initiate curl
 $ch = curl_init();
-
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, "http://tamtech.ddns.net/mktshare/json/?start=20150107&end=20150108&minutes=60&display=%");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// grab URL and pass it to the browser
-$result = curl_exec($ch);
-
-// close cURL resource, and free up system resources
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
 curl_close($ch);
+
+// Will dump a beauty json :3
+//var_dump(json_decode($result, true));
+
 echo $result;
 ?>
